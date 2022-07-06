2bc6ae13254e1b044e687e1daa6c6cb21751ac42
Jarspoot12 <jaredimanol12@gmail.com>
2022-07-06 07:42:07 -0500

    feat: agrrega comandos del módulo global

---
+++ 1-node/1.1-funciones-base/global/index.js
@@ -0,0 +1,5 @@
+console.log('Ruta donde se encunetra el archivo: ',__dirname);
+console.log('Ruta y nombre de archivo: ',__filename);
+//exports : para exportar módulos
+//module: referencia al módulo actual
+//require() : importa un js como módulo
