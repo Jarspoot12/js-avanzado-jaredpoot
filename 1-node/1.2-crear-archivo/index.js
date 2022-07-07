1771ed04fbb51759af8d8bd8785ec664fda65438
Jarspoot12 <jaredimanol12@gmail.com>
2022-07-06 19:59:35 -0500

    feat: crear archivo con node

---
+++ 1-node/1.2-crear-archivo/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const fileName = './my-file.txt';
+const textContent =  'Este texto lo escribí desde node';
+fs.writeFile(fileName, textContent, (err) =>{
+    if(err){
+        throw err;
+    } 
+    else{
+        console.log('El archivo se creó correctamente')
+    }
+});
